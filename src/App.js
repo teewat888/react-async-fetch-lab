8abe879bba0fe683dc761600cf4de4edb7d14cf4
teerawat prarom <teewat@yahoo.com>
2021-09-12 20:09:48 +1000

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+    state = {
+        astroName: []
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                astroName: data.people
+            })
+        }).catch((e) => {console.log(e)});
+    }
+
+    astroList = () => {
+        return this.state.astroName.map((el) => <p key={el.name}>{el.name}</p>)
+    
+    }
+    render() {
+        return <div>{this.astroList()}</div>
+    }
+}
